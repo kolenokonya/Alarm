728f0fbf7ff0569f816dd2404305b4f5aad656e0
KolenoKonya <isip_n.yu.golikov@mpt.ru>
2022-03-19 22:42:10 +0300

    qd

---
+++ app/src/main/java/com/example/alarm/Alarm.java
@@ -0,0 +1,17 @@
+package com.example.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class Alarm extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Бдзыньк", Toast.LENGTH_SHORT).show();
+        Vibrator vibrator=(Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(1000);
+    }
+}
